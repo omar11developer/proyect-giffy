c126c9aaaa4baf802d1890ef1d6354d5960e3e32
omar11developer <omarmenjivar083@gmail.com>
2022-10-10 12:21:38 -0600

    Obteniendo datos de la api de gifts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import getGifts from './services/getGifts';
+
 
 function App() {
+  const [gifts, setGifts] = useState([]);
+
+  useEffect(()=>{
+    getGifts({keyword:'morty'}).then(gifts => setGifts(gifts))
+  
+  },[])
+
   return (
     <div className="App-content">
-      
+      <section>
+       {gifts.map( ItemImg => ( 
+        <img src={ItemImg.url} alt='' />
+       ))}
+      </section>
     </div>
   );
 }
---
+++ src/services/getGifts.js
@@ -0,0 +1,16 @@
+const apikey = '';
+
+export default function getGifts ({keyword = 'morty'} = {}) {
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
+    return fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      const {data} = response
+      const gifs = data.map(image => {
+        const {images, title, id} = image
+        const {url} = images.fixed_height_still
+        return {title, id , url}
+    })
+      return gifs
+    })
+}
