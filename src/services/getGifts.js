738334f17863fac85ac5e194907b6839e6723d9c
omar11developer <omarmenjivar083@gmail.com>
2022-10-12 11:11:51 -0600

    Agregando vaiables de entorno

--- src/services/getGifts.js
+++ src/services/getGifts.js
@@ -1,9 +1,9 @@
 
 
-const apikey = 'zRbLtaug9gSznR5SjuHAJYxe77KBJNm3';
+const apikey = process.env.REACT_APP_API_KEY;
 
 export default function getGifts ({keyword = 'morty'} = {}) {
-
+    console.log(apikey);
     const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
     return fetch(apiURL)
     .then(res => res.json())
